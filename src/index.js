8c68ae017645b5c80e08f4b2802d5e2b9da50ab1
github username <github email address>
2018-04-30 17:39:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Code Goes Here
+// Code Goes Hee
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
